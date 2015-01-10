9c1e8f0aff3eaa4eabc95654781152f02adc4392
Robin Goos <robingoos@gmail.com>
2015-01-10 21:59:43 +0100

    Updated player queuer script

--- player_queuer.py
+++ player_queuer.py
@@ -2,7 +2,7 @@ from src.amqp_client import AMQPClient, CrawlPlayerMessage
 
 def main():
   client = AMQPClient()
-  for id in range(964, 966):
+  for id in range(1, 50001):
     client.publish(CrawlPlayerMessage(id))
   
 if __name__ == '__main__':
